da1a9c313caf2d2e2450bdeff7adeb6d682d1175
Sajjad Hossain <80249167+sajjad960@users.noreply.github.com>
2022-05-22 13:42:56 +0600

    Update stationRoutes.js

--- 4_Api_Server/routes/stationRoutes.js
+++ 4_Api_Server/routes/stationRoutes.js
@@ -5,6 +5,7 @@ const stationController = require('../controllers/stationController');
 
 const router = express.Router();
 
+//Station crud
 router.get('/', stationController.getAllStation);
 router.post('/', stationController.createStation);
 router.patch('/:id', stationController.updateStation);
